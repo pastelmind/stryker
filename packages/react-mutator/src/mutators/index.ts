9074bbeddf1799dc605148a189d2ae48d20d4245
Yehyoung Kang <pastelmind@kaist.ac.kr>
2020-07-08 03:53:25 +0900

    feat: Add LifecycleMethodSwapMutator

--- packages/react-mutator/src/mutators/index.ts
+++ packages/react-mutator/src/mutators/index.ts
@@ -1,5 +1,11 @@
 import JsxAttributeValueNumberMutator from './JsxAttributeValueNumberMutator';
 import JsxChangeNameCaseMutator from './JsxChangeNameCaseMutator';
+import LifeCycleMethodSwapMutator from './LifecycleMethodSwapMutator';
 import StateMutationMutator from './StateMutationMutator';
 
-export const nodeMutators = Object.freeze([new JsxAttributeValueNumberMutator(), new JsxChangeNameCaseMutator(), new StateMutationMutator()]);
+export const nodeMutators = Object.freeze([
+  new JsxAttributeValueNumberMutator(),
+  new JsxChangeNameCaseMutator(),
+  new LifeCycleMethodSwapMutator(),
+  new StateMutationMutator(),
+]);
